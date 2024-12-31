d24fe508660589c3f3da3eaa2ec3548718b8d61c
Daniel Asante-Otchere <danielotchere18@gmail.com>
2024-12-31 15:24:04 +0000

    Added driver class. The driver class encapsulates the username, car string and deployCar method. The constructor initializes an instance of the Driver class with a username and calls the deployCar method. The deployCar method uses the Utilities.bottomCenterMultiLineString static method to bottom center the car string in the console.

---
+++ UberDriverGame/Driver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Driver
+{
+    private string username;
+    private string car =
+            "  .#████#.\r\n" +
+            " .████████.\n" +
+            " :████████:\n" +
+            ".:*      *:.\n" +
+            " .-+*##*+-.\n" +
+            " .=%    %=.\n" +
+            " .████████.\n" +
+            " .████████.\n" +
+            " .=%████%=:\n" +
+            " .%......%:\n" +
+            " .#.    .#.\n" +
+            "  :%████%: ";
+
+    public Driver(string username)
+    {
+        this.username = username;
+        deployCar();
+    }
+
+    private void deployCar()
+    {
+        Utilities.bottomCenterMultiLineString(car);
+        Console.ReadKey();
+    }
+}
