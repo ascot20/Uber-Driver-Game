165603c77d685cac291994ed5d55fe9d5fecce24
Daniel Asante-Otchere <danielotchere18@gmail.com>
2024-12-31 22:19:29 +0000

    Modified Driver class with steering functionality. Added currentLane which tracks the active lane and laneoffsets which contains the horizontal positions for the lanes.steerLeft and steerRight chnages the current lane and changes the car's position.updateCarPosition handles printing the car at the new lane.

--- UberDriverGame/Driver.cs
+++ UberDriverGame/Driver.cs
@@ -5,17 +5,19 @@ class Driver
     private string username;
     private string car =
             "  .#████#.\r\n" +
-            " .████████.\n" +
-            " :████████:\n" +
-            ".:*      *:.\n" +
-            " .-+*##*+-.\n" +
-            " .=%    %=.\n" +
-            " .████████.\n" +
-            " .████████.\n" +
-            " .=%████%=:\n" +
-            " .%......%:\n" +
-            " .#.    .#.\n" +
-            "  :%████%: ";
+            " .████████.\r\n" +
+            " :████████:\r\n" +
+            ".:*      *:.\r\n" +
+            " .-+*##*+-.\r\n" +
+            " .=%    %=.\r\n" +
+            " .████████.\r\n" +
+            " .████████.\r\n" +
+            " .=%████%=:\r\n" +
+            " .%......%:\r\n" +
+            " .#.    .#.\r\n" +
+            "  :%████%:\r\n";
+    private int currentLane = 2;
+    private int[] laneOffsets = { 60,80,100 };
 
     public Driver(string username)
     {
@@ -25,7 +27,42 @@ class Driver
 
     private void deployCar()
     {
-        Utilities.bottomCenterMultiLineString(car);
-        Console.ReadKey();
+        updateCarPosition();
+    }
+
+    public void steerLeft()
+    {
+        if (currentLane > 1)
+        {
+            currentLane -= 1;
+            updateCarPosition();
+        }
+
+    }
+
+    public void steerRight()
+    {
+        if (currentLane < 3)
+        {
+            currentLane += 1;
+            updateCarPosition();
+        }
+    }
+
+    private void updateCarPosition()
+    {
+        Console.Clear();
+
+        int carHeight = car.Split("\n").Length;
+        Utilities.bottomCenterCursor(carHeight);
+
+
+        string[] carParts = car.Split("\n");
+        for (int i = 0; i < carHeight; i++)
+        {
+            string carPart = carParts[i];
+            Console.SetCursorPosition(laneOffsets[currentLane - 1], Console.CursorTop);
+            Console.WriteLine(carPart);
+        }
     }
 }
