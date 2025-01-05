1d32376819074fca3f84ee874c51895d69e1d173
Daniel Asante-Otchere <danielotchere18@gmail.com>
2025-01-05 22:51:50 +0000

    Added ScreenBuffer class to manage and render a 2D array of characters to reduce uneccessary updates. The ScreenBuffer constructor takes a width and height parameter and initializes the buffer's width and height and allocates memory for buffer and previousBuffer arrays with height and width dimensions. The clearBuffer method uses a nested for loop to initialize each cell of buffer and previousBuffer arrays with space character. The writeChar method accepts a BufferChar parameter, a struct with fields xpos(column position), yPos(row position) and character(character to write). It checks that xPos and yPos are within the buffer's dimension with an if statement before updating the buffer array at the specified location. The clearChar method accepts a BufferChar parameter and checks whether the given xPos and yPos are within the buffer's dimensions before updating the location with a space charater. The writeLine method accepts a BufferString parameter, a struct with fields xpos(column position), yPos(row postion) and text(string to write). A for loop is used to loop through the characters in the string and a BufferChar is created and initialized for each character with the column position (xPos) incremented during each iteration.The writeChar method is reused to handle writing a character to the buffer array. clearLine method accepts a BufferString parameter and uses a for loop to iterate each character in the string creating and initializing a BufferChar and reusing the clearChar mehod to clear a character from the buffer. The writeLines method(handles writing multiline strings to buffer) accepts a BufferString parameter. The text string is split into an array of lines using Split method. The loop iterates through the lines creating and initializing a BufferString with yPos incrementing for each line.The writeLine method is reused to write each line to the buffer. clearLines method(removes multiline string from the buffer) accepts a BufferString parameter and uses a for loop to iterate through the lines creating and initializing a BufferString and resusing the clearLine method to clear each line.renderToConsole method(updates the console display) uses a nested for loop to iterate through the buffer dimensions. An if statement is used to check if the character in the buffer differs from the matching cell position in the previousBuffer array. If it diifers the console cursor is moved to the position using Console.SetCurorPosition and the character is written to the console using Console.Write. The previousBuffer cell is updated to match the curent buffer cell. Each method performs a single functionality. Avoided repeatition by reusing methods. Used descriptive method names and variables. Ensure encapsulation by avoiding access to fields and esposing only the methods.

---
+++ UberDriverGame/ScreenBuffer.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+class ScreenBuffer
+{
+    //constants
+    const char spaceCharacter = ' ';
+
+    //fields
+    private int width;
+    private int height;
+    private char[,] buffer;
+    private char[,] previousBuffer;
+    
+
+    public ScreenBuffer(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+        this.buffer = new char[height, width];
+        this.previousBuffer = new char[height, width];
+        this.clearBuffer();
+    }
+
+    //initialize 2D buffer array with space character
+    public void clearBuffer()
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                this.buffer[i, j] = spaceCharacter;
+                this.previousBuffer[i, j] = spaceCharacter;
+            }
+        }
+    }
+
+    //write a character to 2D buffer array
+    public void writeChar(BufferChar c)
+    {
+        if ((c.xPos >= 0 && c.xPos < this.width) && (c.yPos >= 0 && c.yPos < this.height))
+        {
+            this.buffer[c.yPos, c.xPos] = c.character;
+        }
+    }
+
+    //clear a character from 2D buffer array
+    public void clearChar(BufferChar c)
+    {
+        if ((c.xPos >= 0 && c.xPos < this.width) && (c.yPos >= 0 && c.yPos < this.height))
+        {
+            this.buffer[c.yPos, c.xPos] = ' ';
+        }
+    }
+
+    //write a line to the 2D buffer array
+    public void writeLine(BufferString stringText)
+    {
+        int stringLength = stringText.text.Length;
+
+        for (int i = 0; i < stringLength; i++)
+        {
+            BufferChar c;
+            c.xPos = stringText.xPos + i;
+            c.yPos = stringText.yPos;
+            c.character = stringText.text[i];
+
+            writeChar(c);
+        }
+    }
+
+    //clear a line from 2D buffer array
+    public void clearLine(BufferString stringText)
+    {
+        int stringLength = stringText.text.Length;
+
+        for (int i = 0; i < stringLength; i++)
+        {
+            BufferChar c;
+            c.xPos = stringText.xPos + i;
+            c.yPos = stringText.yPos;
+            c.character = spaceCharacter;
+
+            this.clearChar(c);
+        }
+    }
+
+    //write lines to 2D buffer array
+    public void writeLines(BufferString stringText)
+    {
+        string[] lines = stringText.text.Split("\n");
+        int stringHeight = lines.Length;
+
+        for (int i = 0; i < stringHeight; i++)
+        {
+            BufferString c;
+            c.xPos = stringText.xPos;
+            c.yPos = stringText.yPos + i;
+            c.text = lines[i];
+
+            this.writeLine(c);
+        }
+    }
+
+    //clear lines from 2D buffer array
+    public void clearLines(BufferString stringText)
+    {
+        string[] lines = stringText.text.Split("\n");
+        int stringHeight = lines.Length;
+
+        for (int i = 0; i < stringHeight; i++)
+        {
+            BufferString c;
+            c.xPos = stringText.xPos;
+            c.yPos = stringText.yPos + i;
+            c.text = lines[i];
+
+            this.clearLine(c);
+        }
+    }
+
+    //render the 2D buffer array to console
+    public void renderToConsole()
+    {
+        for(int i = 0; i < this.height; i++)
+        {
+            for(int j = 0;j < this.width; j++)
+            {
+                if (this.buffer[i,j] != this.previousBuffer[i, j])
+                {
+                    Console.SetCursorPosition(j,i);
+                    Console.Write(this.buffer[i,j]);
+                    this.previousBuffer[i,j] = this.buffer[i,j];
+                }
+            }
+        }
+    }
+}
+
+struct BufferChar
+{
+    public int xPos;
+    public int yPos;
+    public char character;
+}
+
+struct BufferString
+{
+    public int xPos;
+    public int yPos;
+    public string text;
+}
