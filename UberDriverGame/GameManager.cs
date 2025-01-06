f279eaec4f38a1d31be7adfff984ed3283895f6a
Daniel Asante-Otchere <danielotchere18@gmail.com>
2025-01-06 02:40:32 +0000

    Refactored GameManager. Added handleInGameInput method to handle user input during gameplaying. Used enum to replace numeric values for menu options enhancing readability and maintainability.

--- UberDriverGame/GameManager.cs
+++ UberDriverGame/GameManager.cs
@@ -1,26 +1,78 @@
 ﻿using System;
+using System.Threading;
 
 class GameManager
 {
+    
+
     public GameManager()
     {
+
         Console.CursorVisible = false;
+
         Utilities.checkConsoleSize();
 
         int optionSelected = GameMenus.displayStartMenu();
 
-        switch (optionSelected)
+        switch ((Option)optionSelected)
         {
-            case 0:
-                GameMenus.displayNewGameMenu();
+            case Option.NewGame:
+                string username = GameMenus.displayNewGameMenu();
+                ScreenBuffer screenBuffer = new ScreenBuffer(Utilities.screenWidth, Utilities.screenHeight);
+                Environment environment = new Environment(screenBuffer);
+                Driver driver = new Driver(username, screenBuffer);
+                handleInGameInput(driver, screenBuffer);
                 break;
-            case 1:
+            case Option.LoadGame:
                 Console.WriteLine("Load game");
                 break;
-            case 2:
+            case Option.Exit:
                 Console.WriteLine("Exit game");
                 break;
         }
     }
+
+
+    private void handleInGameInput(Driver driver, ScreenBuffer screenBuffer)
+    {
+
+        ObstacleManager obstacleManager = new ObstacleManager();
+
+        bool isRunning = true;
+
+        while (isRunning)
+        {
+            obstacleManager.moveObstacles(screenBuffer);
+
+            if (Console.KeyAvailable)
+            {
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.A:
+                        driver.steerLeft(screenBuffer);
+                        break;
+
+                    case ConsoleKey.D:
+                        driver.steerRight(screenBuffer);
+                        break;
+
+                }
+            }
+
+            screenBuffer.renderToConsole();
+
+            Thread.Sleep(50);
+
+        }
+    }
+}
+
+enum Option
+{
+    NewGame = 0,
+    LoadGame = 1,
+    Exit = 2
 }
 
