cb51dc59066abf737db49781ed9a59c8ceb4c5ca
Daniel Asante-Otchere <danielotchere18@gmail.com>
2025-01-06 02:15:10 +0000

    Added obstacleManager to manage obstalce creation.

---
+++ UberDriverGame/ObstacleManager.cs
@@ -0,0 +1,36 @@
+﻿class ObstacleManager
+{
+    private string carObstacle1 =
+        " .#████████#.\r\n" +
+        " .██████████.\r\n" +
+        " .#       ##.\r\n" +
+        ".:#####%@@%#:.\r\n" +
+        " .  ██████  .\r\n" +
+        " .%#██████#%.\r\n" +
+        " .  ██████  .\r\n" +
+        " .##########.\r\n" +
+        " .#        #.\r\n" +
+        " .%%      %%.\r\n" +
+        " .%%      %%.\r\n" +
+        " .#        #.\r\n" +
+        " .=+------+=.\r\n";
+    private string carObstacle2 = "";
+    private string carObstacle3 = "";
+    Obstacle dummy;
+
+    public ObstacleManager()
+    {
+        this.dummy = new Obstacle(carObstacle1);
+        
+    }
+
+    public void moveObstacles(ScreenBuffer screenBuffer)
+    {
+        this.dummy.moveObstacle(screenBuffer);
+    }
+
+    //private void clearObstacles(ScreenBuffer screenBuffer)
+    //{
+    //    this.dummy.clearObstacle(screenBuffer);
+    //}
+}
