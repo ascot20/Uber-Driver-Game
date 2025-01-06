86880d422d4961399b5354bf8e88df6f435e7053
Daniel Asante-Otchere <danielotchere18@gmail.com>
2025-01-06 02:13:26 +0000

    Added Obstacle class to render obstacles. The Obstacle constructor instantiates an obect with a carObstacle string and assigns it string to the instance field carObstacle. It generates a random lane number for the current lane within the valid range. It sets the firstRowPosition so that the obstacle starts outside the visible screen area. It initializes carObstacleBuffers as an empty list.moveObstacle method(writes obstacle to the buffer) calls clearObstacle to remove previosly rendered buffer strings from the buffer.It splits the carObstacle string into lines base on the separator. It uses a for loop to loop through each line calculating its position relative to firstposition and placing it within the corresponding lane and add to the list.clearObstacle accepts a ScreenBuffer Obect and uses a for loop to loop through the caObstacleBuffers list and clearing them in the buffer. used descriptive constant names to improve readability and maintainability

--- UberDriverGame/Obstacle.cs
+++ UberDriverGame/Obstacle.cs
@@ -1,4 +1,62 @@
-﻿class Obstacle
+﻿using System;
+using System.Collections.Generic;
+
+class Obstacle
 {
+    //constants
+    private const string separator = "\r\n";
+    private const int minLane = 1;
+    private const int maxLane = 3;
+
+    //fields
+    private string carObstacle;
+    private int currentLane;
+    private int firstRowPosition;
+    private List<BufferString> carObstacleBuffers;
+
+    public Obstacle(string carObstacle)
+    {
+        Random generator = new Random();
+        this.carObstacle = carObstacle;
+        this.currentLane = generator.Next(minLane, maxLane + 1);
+        this.firstRowPosition = -carObstacle.Split(separator).Length;
+        this.carObstacleBuffers = new List<BufferString>();
+    }
+
+    //write obstacle to screen buffer and add to list
+    public void moveObstacle(ScreenBuffer screenBuffer)
+    {
+        this.clearObstacle(screenBuffer);
+
+        BufferString carObstacleBuffer;
+        int carHeight = carObstacle.Split(separator).Length;
+        string[] carParts = carObstacle.Split(separator);
+
+        for (int i = 0; i < carHeight; i++)
+        {
+            int currentRow = firstRowPosition + i;
+
+            if (currentRow >= 0 && currentRow < Console.WindowHeight)
+            {
+                string carPart = carParts[i];
+                carObstacleBuffer.xPos = Environment.laneOffsets[currentLane - 1];
+                carObstacleBuffer.yPos = currentRow;
+                carObstacleBuffer.text = carPart;
+                screenBuffer.writeLine(carObstacleBuffer);
+                carObstacleBuffers.Add(carObstacleBuffer);
+            }
+        }
+        firstRowPosition += 1;
+    }
+
+    //clear buffers in before every move
+    private void clearObstacle(ScreenBuffer screenBuffer)
+    {
+        for (int i = 0; i < carObstacleBuffers.Count; i++)
+        {
+            screenBuffer.clearLine(carObstacleBuffers[i]);
+        }
+        carObstacleBuffers.Clear();
+    }
 }
 
