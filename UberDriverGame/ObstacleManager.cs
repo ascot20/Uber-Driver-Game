21e889b7391f67b1c92a8ad7178cc0e8919244fc
Daniel Asante-Otchere <danielotchere18@gmail.com>
2025-01-07 20:55:57 +0000

    modified ObjectManager class to add functionality for adding and managing new obstacles. The constructor creates an object by assigning the firstObstacleStartingRow with the negative height of the carObstacle string using the split method and Length property. It then allocates memory for the obstacles list. The addObstacle method uses an if else statement to add a new obstacle based on wheter it is the first obstacle or not. The moveObstacles uses a for loop to loop through the obstacles list. It uses an if else statement to determine whether an obstacle has moved beyond the screen height and removes it if it has or moves it with the moveObstacle method if otherwise. Used descriptive names for variables and methods. Avoided hardcorded values by using constants.

--- UberDriverGame/ObstacleManager.cs
+++ UberDriverGame/ObstacleManager.cs
@@ -1,6 +1,10 @@
-﻿class ObstacleManager
+﻿using System.Collections.Generic;
+
+class ObstacleManager
 {
-    private string carObstacle1 =
+    //constants
+    const string separator = "\r\n";
+    private const string carObstacle =
         " .#████████#.\r\n" +
         " .██████████.\r\n" +
         " .#       ##.\r\n" +
@@ -14,19 +18,52 @@
         " .%%      %%.\r\n" +
         " .#        #.\r\n" +
         " .=+------+=.\r\n";
-    private string carObstacle2 = "";
-    private string carObstacle3 = "";
-    Obstacle dummy;
+    private const int spacingMultiplier = 2;
+
+    //fields
+    public List<Obstacle> obstacles;
+    private int firstObstacleStartingRow;
 
     public ObstacleManager()
     {
-        this.dummy = new Obstacle(carObstacle1);
-        
+        this.firstObstacleStartingRow = -carObstacle.Split(separator).Length;
+        this.obstacles = new List<Obstacle>();
+
     }
 
+    //add obstacle to list
+    public void addObstacle()
+    {
+        if (this.obstacles.Count == 0)
+        {
+            this.obstacles.Add(new Obstacle(carObstacle, this.firstObstacleStartingRow));
+        }
+
+        else
+        {
+            Obstacle previousObstacle = this.obstacles[this.obstacles.Count - 1];
+            int previousObstacleRow = previousObstacle.firstRowPosition;
+            int ObstacleStartingRowPos = previousObstacle.firstRowPosition - (previousObstacle.carObstacleHeight * spacingMultiplier);
+
+            this.obstacles.Add(new Obstacle(carObstacle, ObstacleStartingRowPos));
+        }
+    }
+
+    //move obstacles
     public void moveObstacles(ScreenBuffer screenBuffer)
     {
-        this.dummy.moveObstacle(screenBuffer);
+        for (int i = 0; i < this.obstacles.Count; i++)
+        {
+            //if obstacle has moved out of view then remove from list
+            if (this.obstacles[i].firstRowPosition > Utilities.screenHeight)
+            {
+                this.obstacles.RemoveAt(i);
+            }
+            else
+            {
+                this.obstacles[i].moveObstacle(screenBuffer);
+            }
+        }
     }
 
 }
