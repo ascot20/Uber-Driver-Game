ec79cf5574969a2cab42fc054f45b337db3fbf5a
Daniel Asante-Otchere <danielotchere18@gmail.com>
2025-01-09 01:29:35 +0000

    Modified Driver class. Modified updatePosition method to position driver car one row above the maximum screen height by added a bootomOffset to the car's column position. Modified deployCar method to public. Added a new field totalEarnings and modified car string.Used descriptive names for variables to ensure readability and added constant to ensure maintainability.

--- UberDriverGame/Driver.cs
+++ UberDriverGame/Driver.cs
@@ -4,23 +4,25 @@
     const int defaultLane = 2;
     const int minLane = 1;
     const int maxLane = 3;
-    
+    const int bottomOffset = -1;
+
     private const string car =
             "  .#████#.\r\n" +
-            " .████████.\r\n" +
-            " :████████:\r\n" +
-            ".:*      *:.\r\n" +
-            " .-+*##*+-.\r\n" +
-            " .=%    %=.\r\n" +
-            " .████████.\r\n" +
-            " .████████.\r\n" +
-            " .=%████%=:\r\n" +
-            " .%......%:\r\n" +
+            " |████████|\r\n" +
+            " |████████|\r\n" +
+            "_|        |_\r\n" +
+            " |________|\r\n" +
+            " |##    ##|\r\n" +
+            " |████████|\r\n" +
+            " |████████|\r\n" +
+            " |##████##|\r\n" +
+            " |#______#|\r\n" +
             " .#.    .#.\r\n" +
-            "  :%████%:";
+            "  :#████#:";
 
     //fields
     private string username;
+    public decimal totalEarnings;
     public int carHeight = Utilities.getHeightOfString(car);
     public int currentLane;
     private BufferString carBuffer;
@@ -28,11 +30,12 @@
     public Driver(string username, ScreenBuffer screenBuffer)
     {
         this.username = username;
+        this.totalEarnings = 0;
         this.currentLane = defaultLane;
         this.deployCar(screenBuffer);
     }
 
-    private void deployCar(ScreenBuffer screenBuffer)
+    public void deployCar(ScreenBuffer screenBuffer)
     {
         this.updateCarPosition(screenBuffer);
     }
@@ -63,7 +66,8 @@
         this.carBuffer = Utilities.createBottomCenteredBufferString(car);
 
         this.carBuffer.xPos = Environment.getLanePositions()[this.currentLane - 1];
-        
+        this.carBuffer.yPos = this.carBuffer.yPos + bottomOffset;
+
         screenBuffer.writeLines(this.carBuffer);
     }
 }
