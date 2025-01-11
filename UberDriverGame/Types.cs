4c77928dd860a773f65b826e3b01f792c16fc120
Daniel Asante-Otchere <danielotchere18@gmail.com>
2025-01-11 00:48:56 +0000

    Grouped types into a new file for better organization and added a new struct GameEnvironmentVariables to group the games objects.

---
+++ UberDriverGame/Types.cs
@@ -0,0 +1,29 @@
+﻿enum StartMenuOptions
+{
+    NewGame = 0,
+    LoadGame = 1,
+    Exit = 2
+}
+
+struct GameEnvironmentVariables
+{
+    public Driver driver;
+    public ScreenBuffer screenBuffer;
+    public Environment environment;
+    public ObstacleManager obstacleManager;
+    public AccountManager accountManager;
+}
+
+struct BufferChar
+{
+    public int xPos;
+    public int yPos;
+    public char character;
+}
+
+struct BufferString
+{
+    public int xPos;
+    public int yPos;
+    public string text;
+}
